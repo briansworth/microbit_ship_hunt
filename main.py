7779f2ce5ae46f71f8eae70186d4ca4b2aee2988
briansworth <b.farnsworth47@gmail.com>
2021-02-07 21:33:43 -0500

    Add support for multiple ships

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+def generate_ships(count: int):
+    ship_list = [generate_ship()]
+    for i in range(count - 1):
+        ship_list.append(add_ship(ship_list))
+    return ship_list
+
+def add_ship(ships: List[List[tuple(int, int)]]):
+    while True:
+        new_ship = generate_ship()
+        if is_ship_overlapping(new_ship, ships) is False:
+            break
+    return new_ship
+
 def generate_ship():
     vertical = randint(0, 1)
     if vertical == 1:
@@ -5,6 +18,19 @@ def generate_ship():
     else:
         ship = _generate_horizontal_ship()
     return ship
+
+def is_ship_overlapping(ship: List[tuple(int, int)], ships: List[List[tuple(int, int)]]):
+    for coord in ship:
+        for other_ship in ships:
+            if is_coordinate_in_ship(coord, other_ship) is True:
+                return True
+    return False
+
+def is_coordinate_in_ship(coordinate: tuple(int, int), ship: List[tuple(int, int)]):
+    for ship_coord in ship:
+        if coordinate[0] == ship_coord[0] and coordinate[1] == ship_coord[1]:
+            return True
+    return False
 
 def _generate_vertical_ship():
     TOP_EDGE = 0
@@ -60,17 +86,24 @@ def hide_coordinate_led(coordinate: List[number]):
 def show_coordinate_led(coordinate: List[number]):
     led.plot(coordinate[0], coordinate[1])
 
-def make_guess(cursor: tuple(int, int), ship: List[tuple(int, int)]):
+def make_guess(cursor: tuple(int, int), ships: List[List[tuple(int, int)]]):
+    for ship in ships:
+        guess = _make_guess(cursor, ship)
+        if is_guess_a_hit(guess) is True:
+            return guess
+    return guess
+
+def _make_guess(cursor: tuple(int, int), ship: List[tuple(int, int)]):
     is_hit = 0
     if is_coordinate_in_ship(cursor, ship):
         hit()
         is_hit = 1
     return [cursor[0], cursor[1], is_hit]
 
-def is_coordinate_in_ship(coordinate: tuple(int, int), ship: List[tuple(int, int)]):
-    for ship_coord in ship:
-        if coordinate[0] == ship_coord[0] and coordinate[1] == ship_coord[1]:
-            return True
+def is_guess_a_hit(guess: List[number]):
+    HIT_INDEX = 2
+    if guess[HIT_INDEX] == 1:
+        return True
     return False
 
 def hit():
@@ -83,12 +116,6 @@ def show_guesses(guess_list: List[List[number]]):
             _show_hit(guess)
         else:
             _show_guess(guess)
-
-def is_guess_a_hit(guess: List[number]):
-    HIT_INDEX = 2
-    if guess[HIT_INDEX] == 1:
-        return True
-    return False
 
 def _show_hit(guess):
     show_coordinate_led((guess[0], guess[1]))
@@ -129,9 +156,10 @@ def display_game_board(cursor: tuple(int, int), guesses: List[List[number]]):
 # Increase number to reduce difficulty
 GUESS_COUNT = 15
 
+SHIP_COUNT = 2
 CURSOR = (0, 0)
-SHIP = generate_ship()
-WIN_THRESHOLD = SHIP.length
+SHIPS = generate_ships(SHIP_COUNT)
+WIN_THRESHOLD = SHIP_COUNT * 2
 GUESSES: List[List[number]] = []
 
 def on_button_pressed_a():
@@ -147,7 +175,7 @@ def on_button_pressed_b():
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_button_pressed_ab():
-    guess = make_guess(CURSOR, SHIP)
+    guess = make_guess(CURSOR, SHIPS)
     GUESSES.append(guess)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
